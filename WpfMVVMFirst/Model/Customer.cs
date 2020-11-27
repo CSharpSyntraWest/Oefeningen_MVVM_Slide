b3caaa7844cc5de62a39f53f53e3dd2edc2687aa
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-27 14:11:45 +0100

    Add project files.

---
+++ WpfMVVMFirst/Model/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVMFirst.Model
+{
+    class Customer : INotifyPropertyChanged
+    {
+
+        private string name;
+        private int amount;
+        private string country;
+        private int tax;
+        public string Name
+        {
+            get { return name; }
+            set { name = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int Amount
+        {
+            get { return amount; }
+            set
+            {
+                amount = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int Tax
+        {
+            get { return tax; }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfMVVMFirst/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMVVMFirst.Model;
+
+namespace WpfMVVMFirst.ViewModel
+{
+    class CustomerViewModel:BaseViewModel
+    {
+        public CustomerViewModel()
+        {
+            Customer = new Customer() { Name="Bert Boonen",Amount=2000,Country="België"};
+        }
+        private Customer customer;
+        public Customer Customer
+        {
+            get { return customer; }
+            set {
+                customer = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
